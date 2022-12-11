d48c2a0eafb9b25e12f73510e789d35896aac004
GHGFaber <moises.fuentes904@gmail.com>
2022-12-10 16:34:33 -0800

    changes to manage.php

--- manage.php
+++ manage.php
@@ -48,7 +48,7 @@ echo makeTable($rows);
 
 <?php
 $db = get_mysqli_connection();
-$list = $db->prepare("select drinkname from Drinklist" );
+$list = $db->prepare("select drinkname from ViewMT" );
 $list->execute();
 
 $result = $list->get_result();
