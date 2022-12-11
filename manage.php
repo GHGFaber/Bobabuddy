9b5ba0584c047020c11fd77b971c54016c001784
Moises Fuentes <moises.fuentes904@gmail.com>
2022-12-10 18:56:22 -0800

    manage.php changes

--- manage.php
+++ manage.php
@@ -51,7 +51,7 @@ $db = get_mysqli_connection();
 $sql = "select drinkname from ViewMT";
 $result = $db->query($sql);
 while($row = $result->fetch_assoc()){
-    echo $row['drinkname'];
+    echo $row['drinkname'] </br>;
 }
 ?>
 
