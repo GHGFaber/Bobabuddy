095d1a8939b2080462af0e43cace0dfa5710d59e
GHGFaber <moises.fuentes904@gmail.com>
2022-12-10 16:15:57 -0800

    changes to manage.php

--- manage.php
+++ manage.php
@@ -44,17 +44,6 @@ echo makeTable($rows);
 
 </div>
 
-<?php
-$db = get_mysqli_connection();
-$list= $db->prepare("select drinkname from Drinklist where categoryID = 1");
-$list->execute();
-
-$result = $query->get_result();
-$yuh = $result->fetch_all(MYSQLI_ASSOC);
-foreach($yuh as $value) {
-    echo $value['drinkname']. "<br>";
-?>
-
 <p>_______________________________________________<p>
 <p>Milk Teas List<p>
 <p>_______________________________________________<p>
