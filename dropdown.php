c21f95e3f99eeaba853275f4da2a441bb5efa649
Moises Fuentes <moises.fuentes904@gmail.com>
2022-12-12 11:28:54 -0800

    ordering  system

--- dropdown.php
+++ dropdown.php
@@ -17,7 +17,7 @@ $result0 = $query0->get_result();
 
 echo "<select id=DrinkType name=DrinkT onChange='reload()' class='form-control' style='width:100px;'>";
 
-$cat=$_GET['DrinkT'];
+$cat=$_GET['cat'];
 
 echo "<option value='' selected>----Select----</option>";
 while ($category = $result0->fetch_assoc())
