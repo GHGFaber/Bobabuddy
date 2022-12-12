b9081eb371fd4e23e53831fd936cf15eec5aa5a6
Moises Fuentes <moises.fuentes904@gmail.com>
2022-12-12 11:14:13 -0800

    drop down

--- dropdown.php
+++ dropdown.php
@@ -6,6 +6,9 @@ require_once("config.php");
 
 <head>
 <body>
+
+<h2>Place an Order<br></h2>
+<div> Alternate View </br>
 <?php
 $db = get_mysqli_connection();
 $query0 = $db->prepare("SELECT DISTINCT categoryID, categoryName FROM Drinklist");
@@ -13,8 +16,6 @@ $query0->execute();
 $result0 = $query0->get_result();
 $category = $result0->fetch_assoc();
 ?>
-<h2>Place an Order<br></h2>
-<div> Alternate View </br>
 <form method = "post">
 <label for="DrinkT">Choose a category:</label>
 <select name="DrinkT">
